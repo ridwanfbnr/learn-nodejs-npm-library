27c492caba4b7e1c5856546c866ca3534d120d77
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-07-21 20:35:15 +0700

    initial commit

---
+++ number.js
@@ -0,0 +1,16 @@
+
+export const min = (first, second) => {
+    if (first < second) {
+        return first;
+    } else {
+        return second;
+    };
+};
+
+export const max = (first, second) => {
+    if (first > second) {
+        return first;
+    } else {
+        return second;
+    };
+};
